fdc86c80aba8e94e0fb29c98d80636b516e97e11
teleskop150750 <teleskop150750@gmail.com>
2020-11-25 18:11:32 +0300

    first commit

---
+++ WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿
+namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonShow = new System.Windows.Forms.Button();
+            this.textBoxN = new System.Windows.Forms.TextBox();
+            this.labelN = new System.Windows.Forms.Label();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.labelRes = new System.Windows.Forms.Label();
+            this.grid = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonShow
+            // 
+            this.buttonShow.Location = new System.Drawing.Point(31, 75);
+            this.buttonShow.Name = "buttonShow";
+            this.buttonShow.Size = new System.Drawing.Size(229, 28);
+            this.buttonShow.TabIndex = 5;
+            this.buttonShow.Text = "Построить матрицу";
+            this.buttonShow.UseVisualStyleBackColor = true;
+            this.buttonShow.Click += new System.EventHandler(this.buttonShow_Click);
+            // 
+            // textBoxN
+            // 
+            this.textBoxN.Location = new System.Drawing.Point(211, 34);
+            this.textBoxN.Name = "textBoxN";
+            this.textBoxN.Size = new System.Drawing.Size(49, 22);
+            this.textBoxN.TabIndex = 4;
+            // 
+            // labelN
+            // 
+            this.labelN.AutoSize = true;
+            this.labelN.Location = new System.Drawing.Point(28, 37);
+            this.labelN.Name = "labelN";
+            this.labelN.Size = new System.Drawing.Size(119, 17);
+            this.labelN.TabIndex = 3;
+            this.labelN.Text = "Размер матрицы";
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(414, 75);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(177, 28);
+            this.buttonDelete.TabIndex = 7;
+            this.buttonDelete.Text = "Вывести результат";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // labelRes
+            // 
+            this.labelRes.AutoSize = true;
+            this.labelRes.Location = new System.Drawing.Point(411, 37);
+            this.labelRes.Name = "labelRes";
+            this.labelRes.Size = new System.Drawing.Size(0, 17);
+            this.labelRes.TabIndex = 6;
+            // 
+            // grid
+            // 
+            this.grid.BackgroundColor = System.Drawing.SystemColors.ButtonFace;
+            this.grid.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.grid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grid.ColumnHeadersVisible = false;
+            this.grid.Location = new System.Drawing.Point(31, 155);
+            this.grid.Name = "grid";
+            this.grid.RowHeadersVisible = false;
+            this.grid.RowHeadersWidth = 51;
+            this.grid.RowTemplate.Height = 24;
+            this.grid.Size = new System.Drawing.Size(560, 283);
+            this.grid.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 452);
+            this.Controls.Add(this.grid);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.labelRes);
+            this.Controls.Add(this.buttonShow);
+            this.Controls.Add(this.textBoxN);
+            this.Controls.Add(this.labelN);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.grid)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonShow;
+        private System.Windows.Forms.TextBox textBoxN;
+        private System.Windows.Forms.Label labelN;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Label labelRes;
+        private System.Windows.Forms.DataGridView grid;
+    }
+}
+
---
+++ WindowsFormsApp3/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonShow_Click(object sender, EventArgs e)
+        {
+            int n;
+            string inputN = textBoxN.Text;
+            bool resultN = int.TryParse(inputN, out n);
+            if (!resultN || n <= 1)
+            {
+                MessageBox.Show($"Введите число больше 1", "Ошибка");
+                return;
+            }
+
+            grid.RowCount = n;
+            grid.ColumnCount = n;
+
+            var ran = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    grid.Rows[i].Cells[j].Value = ran.Next(-20, 21);
+                }
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            var n = grid.ColumnCount;
+            labelRes.Text = grid.Rows[1].Cells[1].Value.ToString();
+            var s1 = 0;
+            var s2 = 0;
+            for (int i = 0; i < n; i++)
+            {
+                s1 += Int32.Parse(grid.Rows[i].Cells[i].Value.ToString());
+                s2 += Int32.Parse(grid.Rows[i].Cells[n - 1 - i].Value.ToString());
+            }
+
+            if (s1 == s2)
+            {
+                labelRes.Text = "Сумма диагоналей равна";
+            }
+            else
+            {
+                labelRes.Text = s1 > s2 ? "Сумма главной диагонали больше" : "Сумма побочной диагонали больше";
+            }
+        }
+    }
+}
